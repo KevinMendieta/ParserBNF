6ba8ca184ed55a732212da490b3d240165b9a113
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2017-04-18 11:16:35 -0500

    all methods

--- src/ParserD.java
+++ src/ParserD.java
@@ -15,20 +15,74 @@ public class ParserD {
         index++;
     }
     
-    public static void expect(String t){
+    public static void expect(String t) throws Exception{
         if(token.equals(t)){
             nextToken();
         }else{
-            //Throw new Exception();
+            throw new Exception("Was expected" + t + "instead of "+token);
+            //out.write("Was expected" + t + "instead of "+token+"\n");
+        }
+    }
+    
+    public static void pC() throws Exception{
+        pE();
+        if(token.equals("<") || token.equals(">") || token.equals("=") || token.equals(" ")){
+            if(token.equals("<") || token.equals(">")){
+                nextToken();
+                pE();
+            }            
+        }else{
+            throw new Exception("Was expected < > = instead of "+token);
+            //out.write("Was expected < > = instead of "+token+"\n");
+        }
+    }
+    
+    public static void pE() throws Exception{
+        pT();
+        while(token.equals("+") || token.equals("-") || token.equals(" ")){
+            if(token.equals("+") || token.equals("-")){
+                nextToken();
+                pT();
+            }            
+        }
+    }
+    
+    public static void pT() throws Exception{
+        pF();
+        while(token.equals("*") || token.equals("/") || token.equals(" ")){
+            if(token.equals("*") || token.equals("/")){
+                nextToken();
+                pF();
+            }            
+        }
+    }
+    
+    public static void pF() throws Exception{
+        if(token.equals("a") || token.equals("b") || token.equals("c") || token.equals(" ")){
+            if(token.equals("a") || token.equals("b") || token.equals("c")){
+                nextToken();
+            }
+        }else if(token.equals("(") || token.equals(" ")){
+            if(token.equals("(")){
+                nextToken();
+                pE();
+                expect("");
+            }
+        }else{
+            throw new Exception("Was expected a b c ( instead of "+token);
+            //out.write("Was expected a b c ( instead of "+token+"\n");
         }
     }
     
     public static void main(String[] args)throws Throwable{
         while(in.ready()){
             index = 0;
-            String line = in.readLine();
+            line = in.readLine();
             nextToken();
-            
+            pC();
+            expect("$");
+            out.write("Succes "+line+"\n");
         }
+        out.close();
     }        
 }
