ce3ea3bd1167b52a62152a47681119d5a8177f0b
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2017-04-20 15:49:05 -0500

    Functional parser

--- src/ParserD.java
+++ src/ParserD.java
@@ -3,91 +3,94 @@ import java.io.*;
 /**
  * @author KevinMendieta
  */
-public class ParserD {
-    
+public class ParserD{
+
     public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
     public static String token, line;
     public static int index;
-    
+    public static boolean isCorrect;
+
     public static void nextToken(){
-        if(index < line.length())token = Character.toString(line.charAt(index));
+        if(index < line.length()) token = Character.toString(line.charAt(index));
         index++;
     }
-    
+
     public static void skip(){
         while(token.equals(" ")){
             nextToken();
         }
     }
-    
+
     public static void expect(String t) throws Exception{
         if(token.equals(t)){
             nextToken();
-        }else{
+        }else if (isCorrect){
             //out.write("Was expected " + t + " instead of "+token+"\n");
-            System.out.println("Was expected " + t + " instead of "+token+"\n");
+            if (t.equals("$")){
+                out.write("Error: columna " + index + " , fin de entrada inesperado.\n");
+            }else{
+                out.write("Error: columna " + index + " , " + "se esperaba: " + t + "en vez de: " + token + "\n");
+            }
+            isCorrect = false;
         }
     }
-    
+
     public static void pC() throws Exception{
         pE();
         skip();
-        if((token.equals("<") || token.equals(">") || token.equals("=")) && (!token.equals("$"))){
-            if(token.equals("<") || token.equals(">") || token.equals("=")){
-                nextToken();
-                pE();
-            }
-        }else if(!token.equals("$")){
-            //out.write("Was expected < > = instead of "+token+"\n");
-            System.out.println("Was expected < > = instead of "+token+"\n");
+        if((token.equals("<") || token.equals(">") || token.equals("=")) && (!token.equals("$") && isCorrect)){
+            nextToken();
+            pE();
+        }else if(!token.equals("$") && isCorrect){
+            out.write("Error: columna " + index + ", " + token + " caracter inesperado.\n");
+            //System.out.println("Was expected < > = instead of "+token+"\n");
+            isCorrect = false;
         }
     }
-    
+
     public static void pE() throws Exception{
         pT();
         skip();
-        while((token.equals("+") || token.equals("-")) && (!token.equals("$"))){
-            if(token.equals("+") || token.equals("-")){
-                nextToken();
-                pT();
-            }
+        while((token.equals("+") || token.equals("-")) && (!token.equals("$") && isCorrect)){
+            nextToken();
+            pT();
+            skip();
         }
     }
-    
+
     public static void pT() throws Exception{
         pF();
         skip();
-        while((token.equals("*") || token.equals("/")) && (!token.equals("$"))){
-            if(token.equals("*") || token.equals("/")){
-                nextToken();
-                pF();
-            }
+        while((token.equals("*") || token.equals("/")) && (!token.equals("$") && isCorrect)){
+            nextToken();
+            pF();
+            skip();
         }
     }
-    
+
     public static void pF() throws Exception{
         skip();
-        if((token.equals("a") || token.equals("b") || token.equals("c")) && (!token.equals("$"))){
-            if(token.equals("a") || token.equals("b") || token.equals("c")){
-                nextToken();
-            }
-        }else if(!token.equals("$")){
-            //out.write("Was expected a b c ( instead of "+token+"\n");
-            System.out.println("Was expected a b c instead of "+token+"\n");
+        if((token.equals("a") || token.equals("b") || token.equals("c")) && (!token.equals("$") && isCorrect)){
+            nextToken();
+        }else if(!token.equals("$") && isCorrect){
+            out.write("Error: columna " + index + ", " + token + " caracter inesperado.\n");
+            //System.out.println("Was expected a b c instead of "+token+"\n");
+            isCorrect = false;
         }
     }
-    
+
     public static void main(String[] args)throws Throwable{
-        while(in.ready() || true){
+        while(in.ready()){
             index = 0;
+            isCorrect = true;
             line = in.readLine();
             nextToken();
             pC();
             expect("$");
-            System.out.println("Expresion correcta\n");
-            //out.write("Expresion correcta\n");
+            if (isCorrect) out.write("Expresion correcta\n");
         }
         out.close();
-    }        
+    }
+
 }
